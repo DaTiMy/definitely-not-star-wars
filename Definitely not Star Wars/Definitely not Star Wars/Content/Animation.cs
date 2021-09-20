1aea42369fc806034ff9ef2fce36ca92e383bb19
DaTiMy <37105868+DaTiMy@users.noreply.github.com>
2021-09-20 12:40:25 +0200

    Animation 1

---
+++ Definitely not Star Wars/Definitely not Star Wars/Content/Animation.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Definitely_not_Star_Wars
+{
+    public class Animation
+    {
+        public int CurrentFrame { get; set; }
+        public Animation(Texture2D texture, int frameCount)
+        {
+
+        }
+    }
+}
---
+++ Definitely not Star Wars/Definitely not Star Wars/Content/Animation_Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Definitely_not_Star_Wars
+{
+    class Animation_Manager
+    {
+        private Animation _animation;
+    }
+}
